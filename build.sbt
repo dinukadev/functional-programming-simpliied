e41839fa510bee55c83268684bec0411aeee2d40
dinukadev <dinuka.arseculeratne@gmail.com>
2020-10-18 09:07:24 +1100

    first commit of FPS

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "functional-programming-simplified"
+
+version := "0.1"
+
+scalaVersion := "2.13.3"
---
+++ src/main/scala/org/fps/chapter7/SumListInFP.scala
@@ -0,0 +1,12 @@
+package org.fps.chapter7
+
+object SumListInFP extends App {
+
+  def sum(xs: List[Int]): Int  = xs match {
+    case Nil => 0
+    case xs:: tail => xs + sum(tail)
+  }
+
+  println(sum(List(2,4,4)))
+}
+

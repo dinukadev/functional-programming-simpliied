32bc0d75963ef2c20c8fa229ef94076adc51c509
dinukadev <dinuka.arseculeratne@gmail.com>
2020-10-18 11:11:34 +1100

    capitalize with the collection map

--- src/main/scala/chapter13/Capitalize.scala
+++ src/main/scala/chapter13/Capitalize.scala
@@ -7,4 +7,10 @@ object Capitalize extends App {
   val capNames = for(e<-names) yield e.capitalize
 
   println(capNames)
+
+  //With the collection class you can use the map function
+
+  val capWithMap = names.map(_.capitalize)
+
+  println(capWithMap)
 }

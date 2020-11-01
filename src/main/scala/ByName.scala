63b65f460b76f294dff06f7dee9f963e498a28bd
dinukadev <dinuka.arseculeratne@gmail.com>
2020-11-01 14:42:07 +1100

    by name

--- src/main/scala/ByName.scala
+++ src/main/scala/ByName.scala
@@ -1,14 +1,15 @@
 object ByName extends App {
 
-    def timer[A](blockOfCode: => A) = {
-      val startTime = System.nanoTime
-      val result = blockOfCode
-      val stopTime = System.nanoTime
-      val delta = stopTime - startTime
-      (result,delta)
-    }
+  def timer[A](blockOfCode: => A) = {
+    val startTime = System.nanoTime
+    val result = blockOfCode
+    val stopTime = System.nanoTime
+    val delta = stopTime - startTime
+    (result, delta)
+  }
 
-  val sumFunc: (Int,Int)=>Int = (a,b)=> a+b
 
-  print(timer(sumFunc(1,2)))
+  val sumFunc: (Int, Int) => Int = (a, b) => a + b
+
+  print(timer(sumFunc(1, 2)))
 }
